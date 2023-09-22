b667e47faf8f2e40e52cceb2e6a3ab21a6613499
viviane-vieira <112434357+viviane-vieira@users.noreply.github.com>
2023-09-21 23:27:54 -0300

    estilizando tela de vendas

--- tela-vendas/script2.js
+++ tela-vendas/script2.js
@@ -17,3 +17,54 @@ function toggleSidebar()
 
    }
 }
+// Selecionar elementos HTMLda modal cadastro
+const abrirModalBtn = document.getElementById("abrirModal");
+const modalCadastro = document.getElementById("modalCadastro");
+const fecharModalBtn = document.getElementById("fecharModal");
+
+// Função para abrir o modal
+function abrirModal() {
+    modalCadastro.style.display = "block";
+}
+
+// Função para fechar o modal
+function fecharModal() {
+    modalCadastro.style.display = "none";
+}
+
+// Event listeners
+abrirModalBtn.addEventListener("click", abrirModal);
+fecharModalBtn.addEventListener("click", fecharModal);
+
+// Fechar o modal quando clicar fora dele
+window.addEventListener("click", function (event) {
+    if (event.target == modalCadastro) {
+        fecharModal();
+    }
+});
+
+//modal login
+const openModalBtn = document.getElementById("openModalBtn");
+const modal = document.getElementById("loginModal");
+const closeModalBtn = document.querySelector(".close");
+
+// Abre o modal
+openModalBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+// Fecha o modal ao clicar no botão Fechar ou fora do modal
+closeModalBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
+// Evita o fechamento do modal quando se clica dentro dele
+modal.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
