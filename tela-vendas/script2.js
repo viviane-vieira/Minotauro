89da8be92f901cb60d249aaae17a52cd9a783cf4
viviane-vieira <112434357+viviane-vieira@users.noreply.github.com>
2023-09-27 18:11:35 -0300

    validação tela cadastro

--- tela-vendas/script2.js
+++ tela-vendas/script2.js
@@ -17,5 +17,41 @@ function toggleSidebar()
 
    }
 }
+//Validação formulário
+
+const adressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelector("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+//validar input do CEP
+cepInput.addEventListener("keypress",(e) =>{
+    const onlyNumbers = /[0-9]/;
+    const key = String.fromCharCode(e.keyCode);
+//permitir apenas numeros
+ if (!onlyNumbers.test(key)) {
+    e.preventDefault();
+    return;
+ }
+  
+});
+//get address envent
+cepInput.addEventListener("keyup",(e) => {
+   const inputValue = e.target.value; 
+   //checar o valor do input
+   if(inputValue.length === 8){
+    getAndress(inputValue);
+   }
+});
+
+//Get customer address from API
+const getAndress = async (cep) =>{
+    console.log(cep);
+}
 
 
